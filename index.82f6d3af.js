a73b4861a74fd65d73391c3b08e0a4c03e6099a8
Kateryna Shepel <MariyaKaterina@gmail.com>
2025-02-13 13:06:13 +0200

    make build

---
+++ index.82f6d3af.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){var r=e.target.closest(".list-item__link");if(e.preventDefault(),r){var l=r.getAttribute("href");t.setAttribute("src",l)}});
+//# sourceMappingURL=index.82f6d3af.js.map
